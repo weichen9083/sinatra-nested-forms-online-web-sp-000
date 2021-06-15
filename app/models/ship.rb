fd9d7d49f4be6e736ac24c730470459ad2f126d8
wei chen <weichen9083@gmail.com>
2021-06-15 18:14:53 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,13 +1,21 @@
 class Ship
-  attr_accessor :name , :type, :booty 
-  @@all = []
-  
-  def self.all 
-    @@all
-  end 
-  
-  def self.clear 
-    @@all.clear 
-  end 
-  
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
